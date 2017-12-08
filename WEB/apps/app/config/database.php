309dbc0f8e3eb6d0c8e45b600a9dfb288b22ba95
Mas'ul Fauzi <masul.fauzi@gmail.com>
2017-12-09 06:56:44 +0700

    ubah nama database

--- WEB/apps/app/config/database.php
+++ WEB/apps/app/config/database.php
@@ -8,7 +8,7 @@ return array(
         'port' => 3306,
         'user' => 'root',
         'password' => '',
-        'database' => 'timer',
+        'database' => 'lap-timer',
         'tablePrefix' => '',
         'charset' => 'utf8',
         'collate' => 'utf8_general_ci',
