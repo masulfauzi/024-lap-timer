391a6a14e91e30344c9c4c484520f5d0bdde9ee9
Mas'ul Fauzi <masul.fauzi@gmail.com>
2017-12-09 07:04:26 +0700

    delete js dan css yg tidak terpakai

--- WEB/apps/app/views/template/header.php
+++ WEB/apps/app/views/template/header.php
@@ -11,42 +11,16 @@
     
 
     <!-- Bootstrap -->
-    <link href="<?php echo $this->uri->baseUri; ?>vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo $this->uri->baseUri; ?>assets/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
-    <link href="<?php echo $this->uri->baseUri; ?>vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <!-- NProgress -->
-    <link href="<?php echo $this->uri->baseUri; ?>vendors/nprogress/nprogress.css" rel="stylesheet">
-    <!-- iCheck -->
-    <link href="<?php echo $this->uri->baseUri; ?>vendors/iCheck/skins/flat/green.css" rel="stylesheet">
-    <!-- bootstrap-progressbar -->
-    <link href="<?php echo $this->uri->baseUri; ?>vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
-    <!-- JQVMap -->
-    <link href="<?php echo $this->uri->baseUri; ?>vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
-    
+    <link href="<?php echo $this->uri->baseUri; ?>assets/css/font-awesome.min.css" rel="stylesheet">    
     <!-- jQuery -->
-    <script src="<?php echo $this->uri->baseUri; ?>vendors/jquery/dist/jquery.min.js"></script>
+    <script src="<?php echo $this->uri->baseUri; ?>assets/js/jquery.min.js"></script>
     <!-- Bootstrap -->
-    <script src="<?php echo $this->uri->baseUri; ?>vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-    
-    <!-- highchart -->
-	<script src="<?php echo $this->uri->baseUri; ?>vendors/highchart/highcharts.js"></script>
-  	<script src="<?php echo $this->uri->baseUri; ?>vendors/highchart/exporting.js"></script>
-  	
-  	<!-- echart -->
-  	<script src="<?php echo $this->uri->baseUri; ?>js/echart/echarts-all.js"></script>
-  	<script src="<?php echo $this->uri->baseUri; ?>js/echart/green.js"></script>
-
+    <script src="<?php echo $this->uri->baseUri; ?>assets/js/bootstrap.min.js"></script>
     <!-- Custom Theme Style -->
-    <link href="<?php echo $this->uri->baseUri; ?>build/css/custom.min.css" rel="stylesheet">
+    <link href="<?php echo $this->uri->baseUri; ?>assets/css/custom.min.css" rel="stylesheet">
     
-    <!-- Autocomplete -->
-    <link href="<?php echo $this->uri->baseUri; ?>vendors/easyautocomplete/easy-autocomplete.min.css" rel="stylesheet">
-    
-    <!-- Select2 -->
-    <link href="<?php echo $this->uri->baseUri; ?>vendors/select2/select2.min.css" rel="stylesheet">
-    
-    <!-- CKEDITOR -->
-    <script src="<?php echo $this->uri->baseUri; ?>assets/plugin/ckeditor/ckeditor.js"></script>
   </head>
 
   <body class="nav-md" onload="ajax();">
@@ -75,7 +49,7 @@
 		        	foreach($grup_menu as $list_grup_menu)
 		        	{
 			    ?>
-			    <li><a><i class="fa fa-home"></i> <?php echo $list_grup_menu->nama_grup_menu ?> <span class="fa fa-chevron-down"></span></a>
+			    <li><a><i class="<?php echo $list_grup_menu->class ?>"></i> <?php echo $list_grup_menu->nama_grup_menu ?> <span class="fa fa-chevron-down"></span></a>
                   <ul class="nav child_menu" style="display: none">
 	                  
 	              <?php
@@ -84,7 +58,7 @@
 		              foreach($menu as $list_menu)
 		              {
 			      ?>
-			      		<li><a href="<?php echo $this->location($list_menu->controller) ?>"><?php echo $list_menu->nama_menu ?></a></li>
+			      		<li><a href="<?php echo $this->location($list_menu->controller) ?>"><span class="<?php echo $list_menu->class ?>"></span><?php echo $list_menu->nama_menu ?></a></li>
 			      <?php
 		              }
 		          ?>
@@ -124,7 +98,7 @@
 	            <ul class="nav navbar-nav navbar-right">
 	              <li class="">
 	                <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-	                  <img src="https://simpeg2.unnes.ac.id/simpeg_portofolio/load_photo/<?php echo $this->session->getValue('kode_identitas') ?>" alt=""><?php echo $this->mhome->get_nama() ?>
+	                  <img src="" alt="">User
 	                  <span class=" fa fa-angle-down"></span>
 	                </a>
 	                <ul class="dropdown-menu dropdown-usermenu animated fadeInDown pull-right">
